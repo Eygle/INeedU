fa9fbd1981f10573ac5d3e8ef7fb122699f82ce8
Eygle <cavalec.johan@gmail.com>
2015-10-03 20:02:24 +0200

    Add beginning of create demand with ui-bootstrap, glyphicons and jquery

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -3,23 +3,31 @@
 /* App Module */
 
 var INeedUApp = angular.module('INeedUApp', [
-  'ngRoute',
+    'ngRoute',
+    'ui.bootstrap',
 
-  'INeedUControllers',
-  'INeedUFilters',
-  'INeedUServices'
+    'INeedUControllers',
+    'INeedUFilters',
+    'INeedUServices'
 ]);
 //'ngMap'
 
 INeedUApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/home-page.html',
-        controller: 'HomeCtrl'
-      }).
-
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'partials/home-page.html',
+                controller: 'HomeCtrl'
+            }).
+            when('/create-demand', {
+                templateUrl: 'partials/create-demand.html',
+                controller: 'DemandCtrl'
+            }).
+            when('/create-offer', {
+                templateUrl: 'partials/create-offer.html',
+                controller: 'OfferCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
--- web/app/js/controllers.js
+++ web/app/js/controllers.js
@@ -13,6 +13,56 @@ INeedUControllers.controller('HomeCtrl', ['$scope',
         $scope.categories = getCategories();
 
         $scope.query = "";
-        $scope.category = $scope.categories[0].id;
+        $scope.category = $scope.categories[0];
+        console.log($scope.category);
         $scope.remuneration = 0;
+
+
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+    }]);
+
+INeedUControllers.controller('DemandCtrl', ['$scope',
+    function($scope) {
+        $scope.data = {
+            title: "",
+            categoryId: -1,
+            remuneration: 0,
+            duration: 1,
+            durationLabel: "",
+            date: "",
+            periodic: false
+        };
+
+        $scope.categories = getCategories();
+        $scope.category = $scope.categories[0];
+
+        $scope.maxDate = new Date(2020, 5, 22);
+        $scope.minDate = new Date();
+        $scope.status = {
+            opened: false
+        };
+
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.open = function($event) {
+            $scope.status.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
     }]);
